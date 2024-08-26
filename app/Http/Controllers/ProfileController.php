ca1ca2d2260f1ab78f42e0c6ce2f4b04476fc387
Karolis <karolis.vaiciulis123@gmail.com>
2024-08-26 13:22:25 +0300

    Karolis solution to tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,14 @@ class ProfileController extends Controller
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
 
+        $request->user()->update($request->validated());
+
+        if ($request->has('password')) {
+            $request->user()->update([
+                'password' => bcrypt($request->password)
+            ]);
+        }
+
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
 }
